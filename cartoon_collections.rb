8a3203538a48882cd60950ebe106bad7825f2782
Jason Markson <jasonmarkson76@gmail.com>
2020-08-27 20:25:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,6 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-    array.detect do { |cheese| cheese_types.include?(cheese) }
+    { |cheese| array.include?(cheese) }
   end
 end
