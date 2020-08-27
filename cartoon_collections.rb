2ddf80605fa224bae0ea1bc0adf56af45304cc2b
Jason Markson <jasonmarkson76@gmail.com>
2020-08-27 20:04:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls(array)
-  array.any? {|word| word.length > 4}
+def long_planeteer_calls(short_words)
+  short_words.any? {|word| word.length > 4}
 end
 
 def find_the_cheese# code an argument here
