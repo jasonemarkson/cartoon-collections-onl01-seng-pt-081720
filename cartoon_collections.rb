7d9e281612750eec0a7eb8b4744d2f5243ebda02
Jason Markson <jasonmarkson76@gmail.com>
2020-08-27 19:48:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  dwarves.collect do |name|
+  planeteer_calls.collect do |name|
     "#{name.capitalize}!"
   end
 end
